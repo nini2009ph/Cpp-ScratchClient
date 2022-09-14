4b600ae3765f3220d420c1c1e498179b7dd55b3d
nini2009ph <78690148+nini2009ph@users.noreply.github.com>
2022-09-14 17:53:34 +0200

    Update CloudConnection.hpp

--- scratchclient/CloudConnection.hpp
+++ scratchclient/CloudConnection.hpp
@@ -22,23 +22,23 @@ namespace scratchclient {
 	};
 
 	class CloudConnection {
-  std::function<void(std::string, std::string)> set_cloud_variable_lambda;
+  		std::function<void(std::string, std::string)> set_cloud_variable_lambda;
 		ix::WebSocket websocket;
 		int reconnects;
     
-    void connect();
+    		void connect();
 	public:
 		std::vector<std::function<void(CloudVariable)>> on_set_callbacks;
-    std::vector<CloudVariable> cloud_variables;
+    		std::vector<CloudVariable> cloud_variables;
     
 		std::vector<std::thread> callback_threads;
 		std::vector<std::thread> set_threads;
-    std::thread connection_thread;
+    		std::thread connection_thread;
     
 		ScratchSession client;
 		int project_id;
     
-    CloudConnection(ScratchSession& client, int project_id, bool async = false);
+    		CloudConnection(ScratchSession& client, int project_id, bool async = false);
 		~CloudConnection();
     
 		void set_cloud_variable(std::string name, std::string value, bool async = false);
